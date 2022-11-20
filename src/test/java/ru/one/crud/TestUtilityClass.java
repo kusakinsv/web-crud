51af3ff72ad57b611941575c46b0b0741e539d10
Sergei <kusakinsv@mail.ru>
2022-11-20 19:07:23 +0500

    добавлен класс утилит

---
+++ src/test/java/ru/one/crud/TestUtilityClass.java
@@ -0,0 +1,24 @@
+package ru.one.crud;
+
+import ru.one.crud.entity.Book;
+
+public class TestUtilityClass {
+    protected static long testId;
+
+    public static Book createTestBook() {
+        Book book = new Book();
+        book.setName("Тестовая книга");
+        book.setAuthor("Тестовый автор");
+        book.setRelease("2022");
+        book.setPrivateCatalog(true);
+        return book;
+    }
+
+    public static long getTestId() {
+        return testId;
+    }
+
+    public static void setTestId(long testId) {
+        TestUtilityClass.testId = testId;
+    }
+}
